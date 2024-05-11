9e987ee2ce5aef4d05e53d86135ea977763d5c40
Scott Benton <scott@scottbenton.dev>
2024-05-10 20:17:33 -0400

    fix(api): Adding explicit list of allowed methods

--- main.go
+++ main.go
@@ -11,21 +11,22 @@ import (
 )
 
 func main() {
-    log.Println("Starting server...")
-    r := chi.NewRouter()
+	log.Println("Starting server...")
+	r := chi.NewRouter()
 
-    r.Use(middleware.Logger)
-    r.Use(cors.Handler(cors.Options{
-        AllowedOrigins: []string{"http://localhost:*", "https://apps.scottbenton.dev"},
-        AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-        ExposedHeaders:   []string{"Link"},
-        AllowCredentials: false,
-        MaxAge:           300, // Maximum value not ignored by any of major browsers
-    }))
+	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:*", "https://apps.scottbenton.dev"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
-    r.Mount("/campaigns", campaigns.GetCampaignRouter())
+	r.Mount("/campaigns", campaigns.GetCampaignRouter())
 
-    // Start the server
-    log.Println("Server started on port 8080")
-    log.Fatal(http.ListenAndServe(":8080", r))
+	// Start the server
+	log.Println("Server started on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
